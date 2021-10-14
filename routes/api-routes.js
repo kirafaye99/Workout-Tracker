d232708f5ce61da9b460cf58c953f88613a5194f
kirafaye99 <kirademarco99@gmail.com>
2021-10-14 14:42:09 -0700

    finished routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,7 +14,7 @@ router.get("/api/workouts", (req, res) => {
     .catch((err) => {
         res.json(err);
     });
-})
+});
 
 router.get("/api/workouts/range", (req, res) => {
     Workout.aggregate([
@@ -28,16 +28,58 @@ router.get("/api/workouts/range", (req, res) => {
     .catch((err) => {
         res.json(err);
     });
+});
+
+router.get("/api/workouts/:id", (req, res) => {
+    Workout.findOne({
+        _id: req.params.id
+    })
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
+
+
+router.post("/api/workouts/", (req, res) => {
+    Workout.create({
+        exercises: req.body
+    })
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+})
+
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate({
+        _id: req.params.id,
+    },
+        {$push:
+            {exercises: req.body}
+    }
+    )
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
 })
 
 
 //html routes
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/exercise.html"))
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
 });
 
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/stats.html"))
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
 module.exports = router;
